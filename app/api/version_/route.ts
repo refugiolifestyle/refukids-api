6177a84e4f1ee0c0f01bcf38402135cb994b9eec
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2026-03-31 14:13:35 -0300

    ajuste atualizacao changelog

--- app/api/version_/route.ts
+++ app/api/version_/route.ts
@@ -7,6 +7,10 @@ export async function GET() {
     return NextResponse.json({
         data: {
             version: appversion,
+            changelog: [
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                'Suspendisse a gravida diam. Aenean euismod ante sed facilisis efficitur. Nam pulvinar augue a dictum lacinia.',
+            ],
             url: {
                 android: "https://play.google.com/store/apps/details?id=br.com.arefugio.refukids",
                 ios: "https://apps.apple.com/app/br.com.arefugio.refukids"
